11c21d1c106dcdbee3845b49d2122fa20d888bcc
Arlette Pineda <arlettepimo@gmail.com>
2020-01-22 13:16:07 -0800

    Completed flip one card feature.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 
+var gameCards = document.getElementById('gameCards');
+gameCards.addEventListener('click', handleClick);
+
+function handleClick(event){
+  if(event.target.className.indexOf('card-back') === -1){
+    return;
+  }
+  console.log(event.target);
+  event.target.className += " hidden";
+}
